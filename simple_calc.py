298a87df30ab7c1c571ebb6a7e13127b1c5201c0
aghee <agymunzala@gmail.com>
2023-07-26 16:18:04 +0300

    sorted merge conflict

--- simple_calc.py
+++ simple_calc.py
@@ -5,6 +5,7 @@
 #Testing close pull request feature-push from main
 #For some weird reason pull requests are no longer being created in remote when i push changes to branch. What could be the issue?
 #Testing again at 'contribute dropdown'
+#Sorted merge conflict
 def add(a,b):
     c=a+b
     return c
